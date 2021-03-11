248262fe034840acef2d1d561e3cedef52edd922
Nuruddin Prljaca <nuni.prljaca@gmail.com>
2021-03-11 16:12:43 +0100

    Added index.js file to src folder

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var myDecimal = 6.9;
+    var myDecimalE = 2.54e-3;
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
